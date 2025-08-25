01cd5c2f9aacfd125f750d28365c0202b71775cf
IGianetti <ivangianetti@gmail.com>
2025-08-25 18:26:45 -0300

    feat: Se conecta el logo del header a la base de datos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import styles from './Header.module.css'
-import logo from '/images/andina-logo.png'
+import { db } from '../../firebase/firebase'; 
+import { doc, getDoc } from "firebase/firestore"; 
+
+import styles from './Header.module.css';
+
 function Header() {
+  const [logoUrl, setLogoUrl] = useState('');
+
+  useEffect(() => {
+    const fetchLogoUrl = async () => {
+      try {
+        const docRef = doc(db, "configuracion", "logo");
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setLogoUrl(docSnap.data().urlLogo); 
+        } else {
+          console.log("No se encontró el documento del logo en Firestore.");
+        }
+      } catch (error) {
+        console.error("Error al obtener el logo desde Firestore: ", error);
+      }
+    };
+
+    fetchLogoUrl();
+  }, []);
+
   return (
     <header className={styles.header}>
-       <Link to="/">
-      <img src={logo} alt="Andina Labrada Orfebrería" className={styles.logo} />
-       <h2 className={styles.logoTextMobile}>Andina Labrada</h2>
-    </Link>
+      <Link to="/">
+        {logoUrl ? (
+          <img src={logoUrl} alt="Andina Labrada Orfebrería" className={styles.logo} />
+        ) : (
+          <p>Cargando logo...</p>
+        )}
+        <h2 className={styles.logoTextMobile}>Andina Labrada</h2>
+      </Link>
       <nav className={styles.nav}>
         <NavLink to="/" className={({ isActive }) => (isActive ? styles.activeLink : styles.navLink)}>
           Inicio
